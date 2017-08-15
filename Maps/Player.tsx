4914c4f264d1a4ea1ffdc6d0d00f63ae5ae48414
JeremieBeaudoin-Dion <jeremie.beaudoin.dion@gmail.com>
2017-08-15 16:42:55 -0400

    New Maps

---
+++ Maps/Player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Player" tilewidth="48" tileheight="48" tilecount="72" columns="9">
+ <image source="../Resources/charSheet_48.png" width="433" height="384"/>
+</tileset>
---
+++ Maps/RogueLike.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="RogueLikeTileset" tilewidth="48" tileheight="48" spacing="3" tilecount="1860" columns="60">
+ <image source="../Resources/Tileset.png" width="3057" height="1578"/>
+</tileset>
